c973fd0abd7aaa20fddb26676111b7f3d7c91eef
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 10:44:42 +0300

    adding urls

--- awards/urls.py
+++ awards/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('awardshome.urls'), name='home')
+    path('', include('awardshome.urls'), name='home'),
+    path('users', include('awardsusers.urls'), name='users')
 ]
 
 if settings.DEBUG:
