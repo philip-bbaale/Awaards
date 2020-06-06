3bd2022f595774f269be153f360a8d1356f513dd
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 17:04:50 +0300

    adding rating modesl

--- awardsposts/models.py
+++ awardsposts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from djangoratings.fields import RatingField
 
 # Create your models here.
 class Post(models.Model):
@@ -11,6 +11,9 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    design_rating = RatingField(range=10)
+    usability_rating = RatingField(range=10)
+    content_rating = RatingField(range=10)
 
     def __str__(self):
         return self.project_title
