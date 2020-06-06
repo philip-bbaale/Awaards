67d89d29e3e41729bf0a3f0a866d3eae633305df
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 10:21:45 +0300

    adding basic routes and html

--- awardshome/views.py
+++ awardshome/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    return render(request, 'awardshome/home.html')
+
+def about(request):
+    return render(request, 'awardshome/about.html', {'title':'About'})
