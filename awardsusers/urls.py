74c1989ab188b313cc1e03bf4a2eef4e4489e7c6
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 11:33:34 +0300

    adding update profile

--- awardsusers/urls.py
+++ awardsusers/urls.py
@@ -6,6 +6,7 @@ from awardsusers import views as user_views
 urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+    path('update_profile', user_views.update_profile, name='update_profile'),
     path('login/', auth_views.LoginView.as_view(template_name='awardsusers/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='awardsusers/logout.html'), name='logout'),
 ]
--- awardsusers/views.py
+++ awardsusers/views.py
@@ -46,3 +46,26 @@ def profile(request):
     }
 
     return render(request, 'awardsusers/profile.html', context)
+
+@login_required
+@csrf_protect
+def update_profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form,
+    }
+
+    return render(request, 'awardsusers/update_profile.html', context)
