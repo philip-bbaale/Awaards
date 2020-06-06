d2a3ebe12ace14e8bd4ba05d3754f44de53ff932
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 10:57:00 +0300

    addign users urls

---
+++ awardsusers/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path,include
+from awardsusers import views as user_views
+
+urlpatterns = [
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='awardsusers/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='awardsusers/logout.html'), name='logout'),
+]
