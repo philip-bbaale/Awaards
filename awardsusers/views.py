c71fa1e81e7c53b01de28436f49ee10683d52705
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 10:51:43 +0300

    adding users module

--- awardsusers/views.py
+++ awardsusers/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your views here.
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            for user in User.objects.all():
+                 Profile.objects.get_or_create(user=user)
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your Accout has been created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'instagramUsers/register.html', {'form':form})
+
+@login_required
+@csrf_protect
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form,
+    }
+
+    return render(request, 'instagramUsers/profile.html', context)
