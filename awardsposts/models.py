18fbaa11a22f7b2edbce574c906f6e7206944411
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 15:36:08 +0300

    adding Post models

--- awardsposts/models.py
+++ awardsposts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Post(models.Model):
@@ -6,6 +8,9 @@ class Post(models.Model):
     project_image = models.ImageField(upload_to='post_images')
     project_description = models.TextField(max_length=300)
     project_url = models.CharField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.project_title
