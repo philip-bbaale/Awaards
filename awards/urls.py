e605c5eff9b2b52c8e508c1a24438ce5b82ee20b
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 10:42:37 +0300

    adding static ur

--- awards/urls.py
+++ awards/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.urls import path,include
+from awardsusers import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('awardshome.urls'), name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
