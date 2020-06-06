ce85dedb8072a8583f37269b6c79bd9fe6660f16
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 16:33:50 +0300

    adding another blog

--- awardshome/views.py
+++ awardshome/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
+from awardsposts . models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'awardshome/home.html')
+    post = Post.objects.all().order_by('-last_modified')
+
+    context={
+        'posts' : post,
+    }
+    return render(request, 'awardshome/home.html', context)
 
 def about(request):
     return render(request, 'awardshome/about.html', {'title':'About'})
