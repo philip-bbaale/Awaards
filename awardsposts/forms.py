fd5d1099300eabeb26859f9ff7eafba37eb065e7
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 16:04:21 +0300

    adding posts forms

--- awardsposts/forms.py
+++ awardsposts/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class PostForm(forms.Form):
     project_title = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control","placeholder": "Project Title"}))
-    project_image = forms.ImageField(widget=forms.ImageField(attrs={"class": "form-control","placeholder": "Project Image"}))
+    project_image = forms.ImageField()
     project_description = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control","placeholder": "Project Description"}))
     project_url = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control","placeholder": "Link to live Site"}))
