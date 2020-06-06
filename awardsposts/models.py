076604ca428f1c955b09344792cc19e9244fb40c
Pip Bbaale <namugera.philip@gmail.com>
2020-06-06 15:33:32 +0300

    adding Post models

--- awardsposts/models.py
+++ awardsposts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    project_title = models.CharField(max_length=100)
+    project_image = models.ImageField(upload_to='post_images')
+    project_description = models.TextField(max_length=300)
+    project_url = models.CharField()
+
+    def __str__(self):
+        return self.project_title
